b12705a63097d752eeffbf9e234485aa488ba721
Milan Bharanya <milan.bharanya@tundra.com>
2019-12-20 11:36:24 +0100

    Finish working version

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const chatMessages = `
+const chatLines = `
 <Cthon98> hey, if you type in your pw, it will show as stars
 <Cthon98> ********* see!
 <AzureDiamond> hunter2
@@ -16,27 +16,38 @@ const chatMessages = `
 <AzureDiamond> wait, how do you know my pw?
 <Cthon98> er, I just copy pasted YOUR ******'s and it appears to YOU as hunter2 cause its your pw
 <AzureDiamond> oh, ok.
-`.split("\n")
+`.trim().split("\n")
 
-
+const chatElem = document.getElementById("chat")
+const replyDelayMultiplier = 75
+const typingDelay = 220
+const ourUserName = "<AzureDiamond> "
 populate().then(() => { })
 
-async function populate() {
-    for (const message of chatMessages) {
-        message.split("").reduce(async (previousPromise, char) => {
-            return previousPromise.then(() => {
-                return getPromise(char);
-              });
-        }, Promise.resolve());
+async function populate(){
+    for (const chatLine of chatLines) {
+        const matches = chatLine.match(/(<[a-zA-Z0-9]+>\s)(.*)/)
+        const username = matches[1]
+        const message = matches[2]
+        await delay(replyDelayMultiplier * 10)
+        writeToChat(username)
+        if (username == ourUserName){
+            for (const char of message.split("")){
+                await delay(Math.floor(Math.random() * typingDelay))
+                writeToChat(char)
+            }
+        }else{
+            await delay(message.length * replyDelayMultiplier)
+            writeToChat(message)
+        }
+        writeToChat("\n")
     }
 }
 
-function getPromise(char) {
-    return new Promise((resolve, reject) =>
-        window.setTimeout(() => {
-            const chatElem = document.getElementById("chat")
-            chatElem.innerHTML = chatElem.innerHTML + char
-            resolve()
-        }, Math.floor(Math.random() * 10000))
-    )
+function writeToChat(text){
+    chatElem.innerHTML = chatElem.innerHTML + text.replace("<", "&lt;").replace(">", "&gt;")
+}
+
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
